b54e6bdd6305c3a291afa6518c904caabd2f50e2
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-21 18:21:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade_number)
+    @roster[grade_number] ||= []
+    @roster[grade_number] << student_name
+  end
+
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+
+  def sort
+    @roster.each do |grade_number, students_array|
+      students_array.sort!
+    end
+  end
+
+end
